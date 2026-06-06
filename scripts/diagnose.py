4418657792f1e952cd65e7b6931ba66445eb7eca
Rupinder51120 <zeroexistance99@gmail.com>
2026-06-06 21:41:21 +0530

    chore: remove dead diagnose.py script

--- scripts/diagnose.py
+++
@@ -1,6 +0,0 @@
-from rag_eval.benchmarks.benchmark_generator import Benchmark, LexicalOverlapAnalyzer
-
-bm = Benchmark.load("your_existing_benchmark.json")
-analyzer = LexicalOverlapAnalyzer()
-stats = analyzer.analyze_benchmark(bm)
-print(stats)
